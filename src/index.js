524a77de6db4cc3924a2abf0f0494d4b4ef0620c
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-29 20:18:47 +0800

    Ways to call an Component

--- src/index.js
+++ src/index.js
@@ -3,32 +3,17 @@ import ReactDOM from "react-dom/client";
 
 function Greeting() {
   return (
-    <React.Fragment>
-      <div>
-        <h2>Greetings Ma!</h2>
-        <ul>
-          <li>
-            <a href="#">Hello World</a>
-          </li>
-        </ul>
-      </div>
-      <h2>hello world</h2>
-    </React.Fragment>
-
-    //Short-Hand
-    // <>
-    //   <div>
-    //     <h2>Greetings Ma!</h2>
-    //     <ul>
-    //       <li>
-    //         <a href="#">Hello World</a>
-    //       </li>
-    //     </ul>
-    //   </div>
-    //   <h2>hello world</h2>
-    // </>
+    <div>
+      <Person />
+      <Message />
+    </div>
   );
 }
+
+const Person = () => <h2>Ram Goria</h2>;
+const Message = () => {
+  return <p>This is a message</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
