f4373a6a74e208f221b3c6c97220f89227353327
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-29 22:15:32 +0800

    Simple Project File Management

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+// Reference Link:
+// https://www.amazon.com/Best-Sellers-Books/zgbs/books
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Books />
+      <Books />
+      <Books />
+      <Books />
+    </section>
   );
-}
-
-const Person = () => <h2>Ram Goria</h2>;
-const Message = () => {
-  return <p>This is a message</p>;
 };
 
+const Books = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91jbHTNpy6L._AC_UL600_SR600,400_.jpg"
+    alt="House of Flame and Shadow"
+  />
+);
+const Title = () => <h2>House of Flame and Shadow</h2>;
+const Author = () => <h4>Sarah J. Maas</h4>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
