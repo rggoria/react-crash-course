9316ff7478787c440913a1611254f67a3a650929
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-29 18:42:01 +0800

    JSX Rules and Fragments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// function Greeting() {
-//   return (
-//     <div>
-//       <h2>Greetings Ma!</h2>
-//     </div>
-//   );
-// }
+function Greeting() {
+  return (
+    <React.Fragment>
+      <div>
+        <h2>Greetings Ma!</h2>
+        <ul>
+          <li>
+            <a href="#">Hello World</a>
+          </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+    </React.Fragment>
 
-const Greeting = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Greetings Ma!")
+    //Short-Hand
+    // <>
+    //   <div>
+    //     <h2>Greetings Ma!</h2>
+    //     <ul>
+    //       <li>
+    //         <a href="#">Hello World</a>
+    //       </li>
+    //     </ul>
+    //   </div>
+    //   <h2>hello world</h2>
+    // </>
   );
-};
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
