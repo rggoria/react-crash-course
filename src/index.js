13423e0f4e302cbff9c8339fbee446b068d1e700
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-29 18:34:42 +0800

    Difference between Create Element and JSX

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-  return <h2>My First Component</h2>;
-}
+// function Greeting() {
+//   return (
+//     <div>
+//       <h2>Greetings Ma!</h2>
+//     </div>
+//   );
+// }
+
+const Greeting = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Greetings Ma!")
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
