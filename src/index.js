12410d1a378f9d1f077dc143f8c8654d4e2fd9ca
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-29 23:52:13 +0800

    JSX - CSS (inline css)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import book1 from "./assets/images/book-1.jpg";
 
 // Reference Link:
 // https://www.amazon.com/Best-Sellers-Books/zgbs/books
@@ -29,14 +28,15 @@ const Books = () => {
 
 const Image = () => (
   <div>
-    {/* file from public folder */}
     <img src="./images/book-1.jpg" alt="House of Flame and Shadow" />
-    {/* file from public folder */}
-    <img src={book1} alt="House of Flame and Shadow" />
   </div>
 );
 const Title = () => <h2>House of Flame and Shadow</h2>;
-const Author = () => <h4>Sarah J. Maas</h4>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+    Sarah J. Maas
+  </h4>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
