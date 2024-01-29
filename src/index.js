9c92918d988337dafa26f6669585f882ccf4ba5d
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-30 00:02:40 +0800

    JSX - CSS (alternative inline css)

--- src/index.js
+++ src/index.js
@@ -32,11 +32,14 @@ const Image = () => (
   </div>
 );
 const Title = () => <h2>House of Flame and Shadow</h2>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
-    Sarah J. Maas
-  </h4>
-);
+const Author = () => {
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}>Sarah J. Maas</h4>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
