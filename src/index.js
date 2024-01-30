aaff85b5e9610b75563f0ab79f7a0053be263ec7
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-31 00:02:14 +0800

    Props Dynamic Setup

--- src/index.js
+++ src/index.js
@@ -5,33 +5,41 @@ import "./index.css";
 // Reference Link:
 // https://www.amazon.com/Best-Sellers-Books/zgbs/books
 
-const title = "House of Flame and Shadow";
-const author = "Sarah J. Maas";
-const img = "./images/book-1.jpg";
-
-const someFunc = (param1, param2) => {
-  console.log(param1, param2);
+const firstBook = {
+  title: "House of Flame and Shadow",
+  author: "Sarah J. Maas",
+  img: "./images/book-1.jpg",
 };
 
-someFunc("Hello", "Ma!");
+const secondBook = {
+  title: "Atomic Habits",
+  author: "James Clear",
+  img: "https://m.media-amazon.com/images/I/81YkqyaFVEL._SY342_.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Books />
-      <Books />
-      <Books />
-      <Books />
+      <Books
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Books
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const Books = () => {
+const Books = (props) => {
   return (
     <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
