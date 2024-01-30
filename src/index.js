f8bb53f34fc75997de38bd1847f82aa425a4115c
Ram Emerson Goria <goria_reg@gmail.com>
2024-01-31 01:22:40 +0800

    Simple List

--- src/index.js
+++ src/index.js
@@ -5,41 +5,27 @@ import "./index.css";
 // Reference Link:
 // https://www.amazon.com/Best-Sellers-Books/zgbs/books
 
-const firstBook = {
-  title: "House of Flame and Shadow",
-  author: "Sarah J. Maas",
-  img: "./images/book-1.jpg",
-};
+const books = [
+  {
+    title: "House of Flame and Shadow",
+    author: "Sarah J. Maas",
+    img: "./images/book-1.jpg",
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    img: "https://m.media-amazon.com/images/I/81YkqyaFVEL._SY342_.jpg",
+  },
+];
 
-const secondBook = {
-  title: "Atomic Habits",
-  author: "James Clear",
-  img: "https://m.media-amazon.com/images/I/81YkqyaFVEL._SY342_.jpg",
-};
+const names = ["ram", "clifford", "adrian"];
+const newNames = names.map((name, key) => {
+  console.log(name);
+  return <h1 key={key}>{name}</h1>;
+});
 
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Books
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-          sapiente quasi nobis. Voluptates praesentium nemo nisi officia tempora
-          quidem, nostrum ex repudiandae officiis, placeat quod molestiae aut
-          explicabo. Architecto, consectetur?
-        </p>
-        <button>Click Me</button>
-      </Books>
-      <Books
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
 const Books = (props) => {
