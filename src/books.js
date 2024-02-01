946908ad0ff43a5163ef7e196b5b335af62c8c7b
Ram Emerson Goria <goria_reg@gmail.com>
2024-02-01 23:24:31 +0800

    Import and Export Statement

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    title: "House of Flame and Shadow",
+    author: "Sarah J. Maas",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    img: "https://m.media-amazon.com/images/I/81YkqyaFVEL._SY342_.jpg",
+    id: 2,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,51 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
 
 // Reference Link:
 // https://www.amazon.com/Best-Sellers-Books/zgbs/books
 
-const books = [
-  {
-    title: "House of Flame and Shadow",
-    author: "Sarah J. Maas",
-    img: "./images/book-1.jpg",
-    id: 1,
-  },
-  {
-    title: "Atomic Habits",
-    author: "James Clear",
-    img: "https://m.media-amazon.com/images/I/81YkqyaFVEL._SY342_.jpg",
-    id: 2,
-  },
-];
-
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Books {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Books = (props) => {
-  const { img, title, author, getBook, id } = props;
-  const getSingleBook = () => {
-    getBook(id);
-  };
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>Click Me</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
